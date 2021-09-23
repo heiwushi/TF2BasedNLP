b61fb8495733870ebe908ffac14cfa2a7c738b25
heiwushi <447648965@qq.com>
2021-09-23 22:39:28 +0800

    文本预处理1

--- seq2seq.py
+++ seq2seq.py
@@ -1,9 +1,22 @@
 import tensorflow as tf
+from tensorflow.keras.layers.experimental import preprocessing
+
 import io
 import re
 import unicodedata
+import pathlib
+import tensorflow_text as tf_text
 
 import jieba
+
+
+def load_data(path):
+    with open(path, encoding='utf-8') as f:
+        lines = f.readlines()
+        pairs = [line.split('\t')[0:2] for line in lines]
+        english = [preprocess_sentence(e) for e, c in pairs]
+        chinese = [preprocess_sentence(" ".join(list(jieba.cut(c)))) for e, c in pairs]
+    return english, chinese
 
 
 def unicode_to_ascii(s):
@@ -17,56 +30,58 @@ def unicode_to_ascii(s):
     s = s.replace('《', '\'')
     s = s.replace('、', ',')
     s = s.replace('：', ':')
-    return unicodedata.normalize('NFKC', s)
-
+    #return unicodedata.normalize('NFKC', s)
+    return s
 
 def preprocess_sentence(w):
     w = w.lower().strip()
     w = unicode_to_ascii(w)
-    # 在单词与跟在其后的标点符号之间插入一个空格
-    # 例如： "he is a boy." => "he is a boy ."
-    w = re.sub(r"([?.!,¿])", r" \1 ", w)
+    # 在单词与标点符号之间插入一个空格
+    w = re.sub(r"([.?!,:'])", r" \1 ", w)
+    # 除了 (a-z, 汉字, ".", "?", "!", ",")，将所有字符替换为空格
+    w = re.sub(r"([^ a-z.?!,:\u4e00-\u9fa5])", r" ", w)
+    # 将多个空格或\t变成一个空格
     w = re.sub(r'[" "]+', " ", w)
-    # 除了 (a-z, A-Z, ".", "?", "!", ",")，将所有字符替换为空格
-    # w = re.sub(r"[\\\t]+", " ", w)
+    w = re.sub(r"[\\\t]+", " ", w)
     w = w.rstrip().strip()
-    # 给句子加上开始和结束标记
-    # 以便模型知道何时开始和结束预测
-    #w = '<start> ' + w + ' <end>'
+    # 给句子加上开始和结束标记以便模型知道何时开始和结束预测
+    w = '[start] ' + w + ' [end]'
     return w
 
-def create_dataset(path, num_examples=50000):
-    lines = io.open(path, encoding='UTF-8').read().strip().split('\n')
-    word_pairs = [[sentence for sentence in line.split('\t')][0:2] for line in
-                  lines[:num_examples]]
-    en, sp = zip(*word_pairs)
-    en = ['<start> ' + preprocess_sentence(sentence) + ' <end>' for sentence in en]
-    sp = ['<start> ' + preprocess_sentence(' '.join(list(jieba.cut(sentence)))) + ' <end>' for sentence in sp]
-    return en, sp
-
-
-def tokenize(lang):
-    lang_tokenizer = tf.keras.preprocessing.text.Tokenizer(
-      filters='')
-    lang_tokenizer.fit_on_texts(lang)
-    tensor = lang_tokenizer.texts_to_sequences(lang)
-    tensor = tf.keras.preprocessing.sequence.pad_sequences(tensor,
-                                                         padding='post')
-    return tensor, lang_tokenizer
-
-def load_dataset(path, num_examples=None):
-    # 创建清理过的输入输出对
-    targ_lang, inp_lang = create_dataset(path, num_examples)
-    input_tensor, inp_lang_tokenizer = tokenize(inp_lang)
-    target_tensor, targ_lang_tokenizer = tokenize(targ_lang)
-
-    return input_tensor, target_tensor, inp_lang_tokenizer, targ_lang_tokenizer
 
 def main():
-    en, sp = create_dataset('cmn-eng/cmn.txt')
-    print(en[-1])
-    print(sp[-1])
+    english, chinese = load_data('cmn-eng/cmn.txt')
+    print(english[0:5])
+    print(chinese[0:5])
+    english_processor = preprocessing.TextVectorization(max_tokens=5000)
+    chinese_processor = preprocessing.TextVectorization(max_tokens=5000)
+    english_processor.adapt(english)
+    chinese_processor.adapt(chinese)
+    print(english_processor.get_vocabulary()[:10])
+    print(chinese_processor.get_vocabulary()[:10])
+
+
+
+    # BUFFER_SIZE = len(english)
+    # BATCH_SIZE = 2
+    # dataset = tf.data.Dataset.from_tensor_slices((source, target)).shuffle(BUFFER_SIZE)
+    # dataset = dataset.batch(BATCH_SIZE)
+    # #print(dataset)
+    # for example_source_batch, example_target_batch in dataset.take(1):
+    #   print(example_source_batch)
+    #   print(example_target_batch)
+
+
+
+    #
+    # input_text_processor = preprocessing.TextVectorization(
+    #     standardize=tf_lower_and_split_punct,
+    #     max_tokens=max_vocab_size)
 
 
 if __name__ == '__main__':
     main()
+
+    print(re.sub(r"([?.!,¿:'])", r" \1 ", "哈哈!嗯嗯."))
+    print(re.sub(r'[" "]+', " ", "哈哈      嗯嗯"))
+
